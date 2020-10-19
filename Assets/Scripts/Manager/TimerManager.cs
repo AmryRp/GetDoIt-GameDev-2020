d02639c288edb25d02990b736071d514b19a799e
IFA <imamfh24@gmail.com>
2020-10-19 23:41:32 +0700

    Timer Mode with MilliSeconds

--- Assets/Scripts/Manager/TimerManager.cs
+++ Assets/Scripts/Manager/TimerManager.cs
@@ -7,11 +7,13 @@ using UnityEngine.UI;
 public class TimerManager : MonoBehaviour
 {
     public Text timerText;
+    public Text millisecondsText;
     public float timeCountdownInSecond = 90f;
     public UIManager UI;
 
-    private float timer;
+    private float timer = 1;
     public bool isTimeUp;
+    private string minutes, seconds, milliseconds;
 
     // Start is called before the first frame update
     void Start()
@@ -23,27 +25,7 @@ public class TimerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer > 1f)
-        {
-            timer = 0;
-            if (timeCountdownInSecond > 0)
-            {
-                timeCountdownInSecond--;
-                if (timeCountdownInSecond == 10) timerText.color = Color.red;
-                SetTimer();
-            }
-            else
-            {
-                Debug.Log("Time is Up = " + isTimeUp);
-                timer = 0;
-                Debug.Log(isTimeUp);
-                if (isTimeUp)
-                {
-                    TimeUp();
-                }
-            }
-        }
+        SetTimer();
     }
 
     private void TimeUp()
@@ -59,8 +41,54 @@ public class TimerManager : MonoBehaviour
 
     private void SetTimer()
     {
-        string minutes = Mathf.Floor(timeCountdownInSecond / 60).ToString();
-        string seconds = Mathf.Floor(timeCountdownInSecond % 60).ToString();
-        timerText.text = minutes + " : " + seconds;
+        timer -= Time.deltaTime;
+        if (timeCountdownInSecond > -1)
+        {
+            GetTime(timer); // Hitung time
+            
+            if (timer <= 0)
+            {
+                timer = 1;
+            }
+        }
+        else
+        {
+            timer = 0;
+            Debug.Log(isTimeUp);
+            if (isTimeUp)
+            {
+                TimeUp();
+            }
+        }
+    }
+
+    private void GetTime(float timer)
+    {
+        if (timer > 0)
+        {
+            milliseconds = (Mathf.Floor(timer * 100f) % 100).ToString();
+            millisecondsText.text = milliseconds; //Set milisecond
+            return;
+        } else
+        {
+            timeCountdownInSecond--;
+            minutes = Mathf.Floor(timeCountdownInSecond / 60).ToString();
+            seconds = Mathf.Floor(timeCountdownInSecond % 60).ToString();
+
+            int inSeconds = int.Parse(seconds);
+
+            if(inSeconds < 10)
+            {
+                seconds = "0" + seconds;
+            }
+            
+            if (timeCountdownInSecond == 10)
+            {
+                timerText.color = Color.red;
+                millisecondsText.color = Color.red;
+            }
+
+            timerText.text = minutes + " : " + seconds + " : "; // Set Minutes and Seconds
+        }
     }
 }
